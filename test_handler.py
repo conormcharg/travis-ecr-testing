48da16bef720d493b0f85d6f4e7d5bb10d9fa864
conormcharg <conormcharg@live.co.uk>
2021-03-08 14:24:27 +0000

    Added simple test

---
+++ test_handler.py
@@ -0,0 +1,12 @@
+
+import numpy as np
+from app import handler
+import json
+
+def test_handler_output():
+
+    target = np.arange(15).reshape(3, 5).tolist()
+
+    actual = json.loads(handler(None, None)["body"])["Array"]
+    
+    assert actual == target
